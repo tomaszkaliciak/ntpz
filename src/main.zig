f63a76d248d31bf318bdc242f29f33c53339f808
Tomasz Kaliciak <tomasz.kaliciak1@gmail.com>
2024-07-01 19:56:46 +0200

    Resolve FQDN

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
+const net = std.net;
 
 pub fn main() !void {
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const allocator = std.heap.page_allocator;
+    const hostname = "0.pl.pool.ntp.org";
+    const resolutionResult = try net.getAddressList(allocator, hostname, 123);
 
-    try stdout.print("Hello {s}.\n", .{"world"});
-
-    try bw.flush();
+    for (resolutionResult.addrs) |addr| {
+        std.debug.print("{}\n", .{addr});
+    }
 }
