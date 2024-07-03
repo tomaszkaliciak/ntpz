b96d784968d80bd61678b1020fa85fb0f8c10c52
Tomasz Kaliciak <tomasz.kaliciak1@gmail.com>
2024-07-03 21:54:21 +0200

    Send almost correct timestamp to NTP server

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const os = std.os.linux;
 const PF_INET = 2;
 const SOCK_DGRAM = 2;
 
-const NTP_UNIX_OFFSET_IN_NS = 2208988800000000000;
+const NTP_UNIX_OFFSET_IN_S = 2208988800;
 
 const NtpPacket = extern struct {
     li_vn_mode: u8 = 0,
@@ -29,25 +29,37 @@ pub fn main() !void {
 
     const addrs = resolutionResult.addrs;
 
+    if (addrs.len < 1) {
+        return;
+    }
+
     for (addrs) |addr| {
         std.debug.print("{}\n", .{addr});
     }
 
-    const now: u64 = @intCast(std.time.nanoTimestamp());
-    const ntpTimestamp: u64 = now + NTP_UNIX_OFFSET_IN_NS;
+    const now: u64 = @intCast(std.time.timestamp()); // unix time in [s]
+
+    const ntpTimeSeconds: u64 = now + NTP_UNIX_OFFSET_IN_S;
+
+    // NTPTime.fraction = 1 ==> 1/(2^32)s == 0.2ns
+    // let's dont think about it for now
+    const ntpTimeFraction: u64 = 0;
+
+    const ntpTimeStamp: u64 = ntpTimeSeconds << 32 | ntpTimeFraction;
 
     const ntpClientPacket = NtpPacket{
         .li_vn_mode = 0b00_100_011, // LI (Leap Indicator) = 0 (no notif), VN (Version Number) = 4 (NTPv4), Mode = 3 (client mode)
-        .transmitTime = @byteSwap(ntpTimestamp),
+        .transmitTime = @byteSwap(ntpTimeStamp),
     };
+
+    const ntpPacketInBytes: [48]u8 = @bitCast(ntpClientPacket);
+    var buffer = std.mem.zeroes([48]u8);
+    std.mem.copyForwards(u8, &buffer, &ntpPacketInBytes);
+
+    const sockfd: i32 = @intCast(os.socket(PF_INET, SOCK_DGRAM, 0));
 
     const firstAddr = addrs[0].any;
     const firstAddrLen = addrs[0].getOsSockLen();
 
-    var buffer = std.mem.zeroes([48]u8);
-    const ntpPacketInBytes: [48]u8 = @bitCast(ntpClientPacket);
-    std.mem.copyForwards(u8, &buffer, &ntpPacketInBytes);
-
-    const sockfd: i32 = @intCast(os.socket(PF_INET, SOCK_DGRAM, 0));
     _ = os.sendto(sockfd, &buffer, 48, 0, &firstAddr, firstAddrLen);
 }
