ccae9b0ae64319635c99289acb76676f41743b6f
Tomasz Kaliciak <tomasz.kaliciak1@gmail.com>
2024-07-10 21:51:53 +0200

    Parse&print response from NTP server

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,21 @@ const std = @import("std");
 const net = std.net;
 const os = std.os.linux;
 
+const c = @cImport({
+    @cInclude("time.h");
+});
+
 const PF_INET = 2;
 const SOCK_DGRAM = 2;
 
 const NTP_UNIX_OFFSET_IN_S = 2208988800;
+
+const U16_SECOND_FRACTION_IN_MS = 0.01525878;
+const U32_SECOND_FRACTION_IN_MS = 0.00000023283064365386962890625;
+
+const NtpShortFormat = extern struct { seconds: u16 = 0, fraction: u16 = 0 };
+
+const NtpTimestampFormat = extern struct { seconds: u32 = 0, fraction: u32 = 0 };
 
 const NtpPacket = extern struct {
     li_vn_mode: u8 = 0,
@@ -53,27 +64,95 @@ pub fn main() !void {
     };
 
     const ntpPacketInBytes: [48]u8 = @bitCast(ntpClientPacket);
-    var buffer = std.mem.zeroes([48]u8);
-    std.mem.copyForwards(u8, &buffer, &ntpPacketInBytes);
+    var buffer_out = std.mem.zeroes([48]u8);
+    std.mem.copyForwards(u8, &buffer_out, &ntpPacketInBytes);
 
     const sockfd: i32 = @intCast(os.socket(PF_INET, SOCK_DGRAM, 0));
-
-    const addr = std.net.Address.initIp4(.{ 0, 0, 0, 0 }, 1996);
-    _ = os.bind(sockfd, &addr.any, addr.getOsSockLen());
 
     const firstAddr = addrs[0].any;
     const firstAddrLen = addrs[0].getOsSockLen();
 
-    _ = os.sendto(sockfd, &buffer, 48, 0, &firstAddr, firstAddrLen);
+    _ = os.sendto(sockfd, &buffer_out, 48, 0, &firstAddr, firstAddrLen);
 
-    var buffer2: [48]u8 = undefined;
+    var buffer_in: [48]u8 = undefined;
     var srcAddr: net.Address = undefined;
     var srcAddrLen: os.socklen_t = @sizeOf(net.Address);
 
-    const recv_result = os.recvfrom(sockfd, &buffer2, 48, 0, &srcAddr.any, &srcAddrLen);
+    const recv_result = os.recvfrom(sockfd, &buffer_in, 48, 0, &srcAddr.any, &srcAddrLen);
 
     std.debug.print("Received {}: {x}\n", .{
         recv_result,
-        buffer2[0..recv_result],
+        buffer_in[0..recv_result],
     });
+
+    const packetFromServer = parsePacket(buffer_in);
+
+    std.debug.print("Received li_vn_mode {b}\n", .{packetFromServer.li_vn_mode});
+    std.debug.print("Received stratum {}\n", .{packetFromServer.stratum});
+    std.debug.print("Received pollInterval {}\n", .{packetFromServer.pollInterval});
+    std.debug.print("Received precision {}\n", .{packetFromServer.precision});
+    std.debug.print("Received rootDelay {b}\n", .{packetFromServer.rootDelay});
+    std.debug.print("Received rootDispersion {}\n", .{packetFromServer.rootDispersion});
+    std.debug.print("Received referenceId {}\n", .{packetFromServer.referenceId});
+    std.debug.print("Received referenceTime {}\n", .{packetFromServer.receiveTime});
+
+    const rootDelay = parseNtpShort(packetFromServer.rootDelay);
+    std.debug.print("Received rootDelay {}s {} ms\n", .{ rootDelay.seconds, u16SecondFranctionToMs(@floatFromInt(rootDelay.fraction)) });
+
+    const rootDispersion = parseNtpShort(packetFromServer.rootDispersion);
+    std.debug.print("Received rootDispersion {}s {} ms\n", .{ rootDispersion.seconds, u16SecondFranctionToMs(@floatFromInt(rootDispersion.fraction)) });
+
+    const referenceTime = parseNtpTimestamp(packetFromServer.referenceTime);
+    std.debug.print("Received referenceTime {}s {} ms\n", .{ referenceTime.seconds, u32SecondFranctionToMs(@floatFromInt(referenceTime.fraction)) });
+
+    const originTime = parseNtpTimestamp(packetFromServer.originTime);
+    std.debug.print("Received originTime {}s {} ms\n", .{ originTime.seconds, u32SecondFranctionToMs(@floatFromInt(originTime.fraction)) });
+
+    const receiveTime = parseNtpTimestamp(packetFromServer.receiveTime);
+    std.debug.print("Received receiveTime {}s {} ms\n", .{ receiveTime.seconds, u32SecondFranctionToMs(@floatFromInt(receiveTime.fraction)) });
+
+    const transmitTime = parseNtpTimestamp(packetFromServer.transmitTime);
+    std.debug.print("Received transmitTime {}s {} ms\n", .{ transmitTime.seconds, u32SecondFranctionToMs(@floatFromInt(transmitTime.fraction)) });
 }
+
+fn u16SecondFranctionToMs(fraction: f32) f32 {
+    return U16_SECOND_FRACTION_IN_MS * fraction;
+}
+
+fn u32SecondFranctionToMs(fraction: f32) f32 {
+    return U32_SECOND_FRACTION_IN_MS * fraction;
+}
+
+fn parseNtpShort(payload: u32) NtpShortFormat {
+    var ntpTime = NtpShortFormat{};
+    ntpTime.seconds = @intCast(payload >> 16);
+    ntpTime.fraction = @intCast(payload & 0xFFFF);
+    return ntpTime;
+}
+
+fn parseNtpTimestamp(payload: u64) NtpTimestampFormat {
+    var ntpTime = NtpTimestampFormat{};
+    ntpTime.seconds = @intCast(payload >> 32);
+    ntpTime.fraction = @intCast(payload & 0xFFFFFFFF);
+    return ntpTime;
+}
+
+fn parsePacket(payload: [48]u8) NtpPacket {
+    var packet = NtpPacket{};
+    packet.li_vn_mode = payload[0];
+    packet.stratum = payload[1];
+    packet.pollInterval = payload[2]; // OK - 2 = 8s? check format
+    packet.precision = @bitCast(payload[3]); // -24 , check format
+    packet.rootDelay = std.mem.readInt(u32, payload[4..8], std.builtin.Endian.big);
+    packet.rootDispersion = std.mem.readInt(u32, payload[8..12], std.builtin.Endian.big);
+    packet.referenceId = std.mem.readInt(u32, payload[12..16], std.builtin.Endian.big);
+    packet.referenceTime = std.mem.readInt(u64, payload[16..24], std.builtin.Endian.big);
+    packet.originTime = std.mem.readInt(u64, payload[24..32], std.builtin.Endian.big);
+    packet.receiveTime = std.mem.readInt(u64, payload[32..40], std.builtin.Endian.big);
+    packet.transmitTime = std.mem.readInt(u64, payload[40..], std.builtin.Endian.big);
+    return packet;
+
+    // 0011 1010
+}
+
+// 0000 0010 0010 0011
